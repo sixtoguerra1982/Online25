c0e699a4aa6635f629d8598f8a9995150b834d57
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2017-11-09 01:19:15 -0300

    camino a y camino b

--- ajax_two_B/app/controllers/comments_controller.rb
+++ ajax_two_B/app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
 
   def create
   	@comment = Comment.create(comment_params)
+  	render json: @comment.as_json
   end
 
   private def comment_params
